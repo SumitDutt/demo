8bd83b044f7aa821406ce9da3217abdfad2a3d98
Sumit Dutt Tripati <sumitdutt85@gmail.com>
2025-01-10 16:14:47 +0530

    get and post method

---
+++ src/main/java/com/example/firstproject/bean/Student.java
@@ -0,0 +1,50 @@
+package com.example.firstproject.bean;
+
+public class Student {
+    Long studentId;
+    String name ;
+    String cotact_no;
+    String email_id;
+    String location;
+
+    public Long getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Long studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCotact_no() {
+        return cotact_no;
+    }
+
+    public void setCotact_no(String cotact_no) {
+        this.cotact_no = cotact_no;
+    }
+
+    public String getEmail_id() {
+        return email_id;
+    }
+
+    public void setEmail_id(String email_id) {
+        this.email_id = email_id;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+}
---
+++ src/main/java/com/example/firstproject/respose/FirstProectResponse.java
@@ -0,0 +1,17 @@
+package com.example.firstproject.respose;
+
+public class FirstProectResponse {
+    private String message;
+
+    public FirstProectResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
